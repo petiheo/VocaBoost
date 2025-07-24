420150f0d959edfca0f06719e43b46699dddd0f6
petiheo <158324689+itshoang2024@users.noreply.github.com>
2025-07-24 07:39:42 +0700

    feat(core): Add teacher route and auth middleware

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const authRouter = require('./auth.route');
+const teacherRouter = require('./teacher.route');
 const userRouter = require('./user.route');
 const vocabularyRouter = require('./vocabulary.route');
 const classroomRouter = require('./classroom.route');
@@ -9,6 +10,7 @@ const reviewRouter = require('./review.route');
 const adminRouteer = require('./admin.route');
 
 router.use('/auth', authRouter);
+router.use('/teacher', teacherRouter);
 router.use('/user', userRouter);
 router.use('/vocabulary', vocabularyRouter);
 router.use('/classroom', classroomRouter);
