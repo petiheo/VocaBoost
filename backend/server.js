7f1d8d52917b1b5abbc18d060c702c78094438c3
itshoang2024 <158324689+itshoang2024@users.noreply.github.com>
2025-08-04 01:18:15 +0700

    feat(deploy): setup backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const logger = require('./src/utils/logger');
 const PORT = process.env.PORT || 3000;
 
-const app = require('./src/app');
+const app = require('./src/api');
 app.listen(PORT, () => {
   logger.info(`URL of app: http://127.0.0.1:${PORT}/`);
 });
